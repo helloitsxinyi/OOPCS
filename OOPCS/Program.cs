239d9fa220bcd769d451a39e59ff5b21f972da9f
Xinyi <xinzwerkz@gmail.com>
2021-08-17 10:10:30 +0800

    Add OOPCSDay1 files

---
+++ OOPCS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOPCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
