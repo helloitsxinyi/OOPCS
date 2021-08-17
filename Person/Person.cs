a2085ac1289bcc6e5a0b591f20b131d0a187f9f4
Xinyi Tay <65388923+xinyitay@users.noreply.github.com>
2021-08-17 22:59:50 +0800

    Add Person project and class

---
+++ Person/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Person
+{
+    public class Person
+    {
+        string firstName, lastName;
+        int age;
+        float height;
+        
+        public Person(string firstName, string lastName, int age, float height)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.height = height;
+        }
+
+        public void sayHi()
+        {
+            Console.WriteLine($"Hi, I'm {firstName} {lastName}!");
+        }
+
+        public void sayAge()
+        {
+            Console.WriteLine($"I'm {age} years old");
+        }
+
+        public void sayHeight()
+        {
+            Console.WriteLine($"My height is {height}m");
+        }
+    }
+}
---
+++ Person/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Person
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person gary = new Person("Gary", "Tan", 35, 1.7f);
+            gary.sayHi();
+            gary.sayAge();
+            gary.sayHeight();
+
+            // new line for output separation
+            Console.WriteLine("");
+
+            Person jane = new Person("Jane", "Yong", 23, 1.6f);
+            jane.sayHi();
+            jane.sayAge();
+            jane.sayHeight();
+        }
+    }
+}
