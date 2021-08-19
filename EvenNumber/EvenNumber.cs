a699042facd830b9cdb3fff697192eeb21aaf3e9
Xinyi Tay <65388923+xinyitay@users.noreply.github.com>
2021-08-19 16:05:54 +0800

    Add EvenNumber project

---
+++ EvenNumber/EvenNumber.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace EvenNumber
+{
+    public class EvenNumber
+    {
+        private int evenNo;
+
+        public EvenNumber()
+        {
+            evenNo = 0;
+        }
+
+        private bool IsEven(int n)
+        {
+            if (n % 2 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool SetEvenNumber(int n)
+        {
+            if (IsEven(n))
+            {
+                evenNo = n;
+                return true;
+            }
+            return false;
+        }
+
+        public int GetValue()
+        {
+            return evenNo;
+        }
+    }
+}
---
+++ EvenNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace EvenNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = { 11, 9, 8, 2, 5, 12 };
+
+            EvenNumber num = new EvenNumber();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (num.SetEvenNumber(data[i]))
+                {
+                    Console.WriteLine("Update succeeded.");
+                    Console.WriteLine("Value is now " + num.GetValue() + ".");
+                }
+                else
+                {
+                    Console.WriteLine("Update failed.");
+                    Console.WriteLine(data[i] + " is not even.");
+                }
+
+                Console.WriteLine("");
+            }
+        }
+    }
+}
