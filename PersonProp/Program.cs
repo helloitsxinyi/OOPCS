6bb3072422c5944f01f40e23d3d28839e468d0be
Xinyi Tay <65388923+xinyitay@users.noreply.github.com>
2021-08-19 16:48:21 +0800

    Add PersonProp project

---
+++ PersonProp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PersonProp
+{
+class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonProp gary = new PersonProp("Gary", "Tan", 35, 1.7f);
+
+            // calling properties here, using setter & getter.
+            // not directly changing values of attributes!
+            gary.LastName = "Wong";
+            gary.Age = 36;
+            gary.Height = -1.71f;
+
+            Console.WriteLine("Name: " + gary.FirstName + " " + gary.LastName);
+            Console.WriteLine("Age: " + gary.Age);
+            Console.WriteLine("Height: " + gary.Height + "m");
+
+        }            
+    }
+}
+
