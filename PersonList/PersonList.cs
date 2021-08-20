2fe99bad4d1251f77b74d4ab032e695907118a3d
Xinyi Tay <65388923+xinyitay@users.noreply.github.com>
2021-08-20 12:23:44 +0800

    Add PersonList project files

---
+++ PersonList/PersonList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace PersonList
+{
+    public class PersonList
+    {
+        List<PersonProp> persons;
+
+        public PersonList()
+        {
+            persons = new List<PersonProp>();
+                
+        }
+
+        public bool AddUniqueNamedPerson(PersonProp person)
+        {
+            //foreach (PersonProp p in persons)
+            // {
+            // contains could be useful, but for now we only take in 4 arguments, not 2, need to overload PersonProp class if needed
+            //if persons.Contains(new PersonProp(person.FirstName.ToLower(), person.LastName.ToLower()))
+            // another method
+            //if (p.FirstName.ToLower() == person.FirstName.ToLower())
+
+            if (persons.Find(p => p.FirstName.ToLower() == person.FirstName.ToLower() && p.LastName.ToLower() == person.LastName.ToLower()) == null)
+            {
+                persons.Add(person);
+                return true;
+            }
+            return false;    
+        }
+
+        public
+
+
+    }
+}
---
+++ PersonList/PersonProp.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+{
+    public class PersonProp
+    {
+
+        private string firstName, lastName;
+        private int age;
+        private float height;
+
+
+        public PersonProp(string firstName, string lastName, int age, float height)
+        {
+            //updating an automatic property from within a class itself
+            FirstName = firstName;
+            LastName = lastName;
+            this.age = age;
+            this.height = height;
+        }
+
+        // C# properties
+        // important to rename to FirstName and LastName: automatic properties help to automatically read & update.
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age
+        {
+            get { return age; }
+
+            set
+            {
+                if (value > 0 && value < 150)
+                    age = value;
+            }
+        }
+        public float Height
+        {
+            get { return height; }
+
+            set
+            {
+                if (value > 0 && value < 3.0)
+                    height = value;
+            }
+        }
+
+    }
+
+}
+
