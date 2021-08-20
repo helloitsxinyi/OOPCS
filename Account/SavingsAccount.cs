367149457449555bdc4a88ad03ca0772f16457d4
Xinyi <xinzwerkz@gmail.com>
2021-08-20 21:46:52 +0800

    Add SavingsAccount class

---
+++ Account/SavingsAccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Account
+{
+    public class SavingsAccount : Account
+    {
+        public SavingsAccount(string acctNumber, string acctHolderId, double balance) : base(acctNumber, acctHolderId, balance)
+        {
+
+            public void CreditInterest()
+            {
+
+            }
+        }
+    }
+}
