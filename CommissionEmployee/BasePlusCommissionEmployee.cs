95ec45a0232eabf769e8a6903c6e8c59117bb48e
Xinyi <xinzwerkz@gmail.com>
2021-08-20 15:18:28 +0800

    Update comments to improve clarity

--- CommissionEmployee/BasePlusCommissionEmployee.cs
+++ CommissionEmployee/BasePlusCommissionEmployee.cs
@@ -3,12 +3,13 @@ namespace CommissionEmployee
 {
     // inherit CommissionEmployee class.
     // inherit variables and methods
-    // must include arguments in constructor for base class
+
     public class BasePlusCommissionEmployee : CommissionEmployee
     {
         private double salary;
 
-        //constructors of a base class are not inherited.
+        // constructors of a base class are not inherited. need to make your own. use BASE keyword.
+        // must include arguments in constructor for base class
         // ask parent to initialization for me using base(name, identityNumber....)
         public BasePlusCommissionEmployee(string name, string identityNumber, double grossSales, double commissionRate, double salary): base(name, identityNumber, grossSales, commissionRate)
         {
