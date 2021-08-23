32ef2d04971338aceb1c351f117b05654d552e51
Xinyi <xinzwerkz@gmail.com>
2021-08-23 17:03:56 +0800

    Complete implementation for curr account

--- Account/CurrentAccount.cs
+++ Account/CurrentAccount.cs
@@ -4,18 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OOPCS_Workshop_Inheritance_PartII
+namespace Account
 {
    /**
     * This class should derive from class Account.
     * Current Account earns 0.25% interest, but balance can not be negative
     */
-   class CurrentAccount
+   class CurrentAccount : Account
    {
-      /**
-       * TODO: attributes
-       * Add instance or static attribute when neccessary
-       */
+        /**
+         * TODO: attributes
+         * Add instance or static attribute when neccessary
+         */
+        public static double currInterest = 0.25;
 
 
 
@@ -26,63 +27,65 @@ namespace OOPCS_Workshop_Inheritance_PartII
        * of its parent's constructor to initialize
        * its attributes
        */
+      public CurrentAccount(string acctNumber, string acctHolderId, double balance): base(acctNumber, acctHolderId, balance)
+        {
+
+        }
+
+
+        /**
+         * TODO: implement method CalculateInterest
+         * The method return the interest of this account.
+         * Current Account earns 0.25% interest of its balance.
+         * 
+         * You may need to override this method from
+         * its parent if neccessary. You may even consider moving
+         * the whole method to its parents if possible.
+         */
+        public override double CalculateInterest()
+        {
+            return (currInterest / 100) * Balance;
+        }
+
+
+        /**
+         * TODO: override method Widthraw
+         * This method decreases the account balance
+         * by the given amount. 
+         * For a Current Accounts, balance cannot be negative
+         * 
+         * You may need to override this method from
+         * its parent if neccessary. You may even consider moving
+         * the whole method to its parents if possible.
+         */
+        public override bool Withdraw(double amt)
+        {           
+            if (amt > Balance)
+            {
+                Console.WriteLine("Insufficient balance!");
+                return false;
+            }
+            return base.Withdraw(amt);            
+        }
 
 
 
 
-      /**
-       * TODO: implement method CalculateInterest
-       * The method return the interest of this account.
-       * Current Account earns 0.25% interest of its balance.
-       * 
-       * You may need to override this method from
-       * its parent if neccessary. You may even consider moving
-       * the whole method to its parents if possible.
-       */
+        /**
+         * TODO: override method ToString
+         * This method returns the values of the attributes
+         * of the current object in a more readable format
+         * For example: 
+         * (CurrentAccount) Account: accountNumber=S0000111, accountHolder=S1111111A, balance=2000
+         * 
+         * This method should make use of its parent's method
+         */
+        public override string ToString()
+        {
+            return "(Current Account) " + base.ToString();
+        }
 
 
 
-
-      /**
-       * TODO: implement method CreditInterest
-       * The method deposit the interst amount, returns by
-       * CalculateInterest() method of this
-       * account to its balance.
-       * 
-       * You may need to override this method from
-       * its parent if neccessary. You may even consider moving
-       * the whole method to its parents if possible.
-       */
-
-
-
-
-      /**
-       * TODO: override method Widthraw
-       * This method decreases the account balance
-       * by the given amount. 
-       * For a Current Accounts, balance cannot be negative
-       * 
-       * You may need to override this method from
-       * its parent if neccessary. You may even consider moving
-       * the whole method to its parents if possible.
-       */
-
-
-
-
-      /**
-       * TODO: override method ToString
-       * This method returns the values of the attributes
-       * of the current object in a more readable format
-       * For example: 
-       * (CurrentAccount) Account: accountNumber=S0000111, accountHolder=S1111111A, balance=2000
-       * 
-       * This method should make use of its parent's method
-       */
-
-
-
-
-   }
+    }
 }
