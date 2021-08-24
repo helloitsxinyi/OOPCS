0b63ca5826f586b2c4a777a3421c2dc141a36854
Xinyi <xinzwerkz@gmail.com>
2021-08-24 11:18:58 +0800

    Complete implementation of circle class

---
+++ AbstractClass/Circle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace AbstractClass
+{
+    public class Circle : Shape
+    {
+        private double radius;
+
+        public Circle(string color, double radius) : base(color)
+        {
+            this.radius = radius;
+        }
+
+        public override double Area()
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+    }
+}
