cf16efccb23dc87196d69bcab92b026b6d5860dc
Xinyi <xinzwerkz@gmail.com>
2021-08-24 11:10:56 +0800

    Add triangle class

---
+++ AbstractClass/Triangle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace AbstractClass
+{
+    public class Triangle : Shape
+    {
+        private double width, height;
+
+        public Triangle(string color, double width, double height) : base(color)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public override double Area()
+        {
+            return 0.5 * width * height;
+        }
+    }
+}
