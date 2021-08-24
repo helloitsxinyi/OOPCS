ecbbf899eb49c070c678d8900c952ad1edb6a6bd
Xinyi <xinzwerkz@gmail.com>
2021-08-24 15:52:45 +0800

    Complete implementation for Contact class

---
+++ Interface/Contact.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Interface
+{
+    public class Contact : Item, ISyncable
+    {
+        private string name, mobile;
+        public Contact(string name, string mobile) : base()
+        {
+            this.name = name;
+            this.mobile = mobile;
+        }
+
+        public string GetSyncString()
+        {
+            return $"CO*{id}*{name}*{mobile}";
+        }
+    }
+}
