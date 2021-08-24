553e315ec8465e0c8701ba1ca858a6878234bcc6
Xinyi <xinzwerkz@gmail.com>
2021-08-24 14:38:02 +0800

    Complete implementation for Email

---
+++ Interface/Email.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Interface
+{
+    public class Email : Item, ISyncable
+    {
+        private string title, from, content;
+
+        public Email(string title, string from, string content) : base()
+        {
+            this.title = title;
+            this.from = from;
+            this.content = content;           
+        }
+
+        public string GetSyncString()
+        {
+            return $"EM*{id}*{title}*{from}*{content}";
+        }
+    }
+}
