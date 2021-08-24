7fd06d896f1cb2658996a71f77094cdd03fcdd9d
Xinyi <xinzwerkz@gmail.com>
2021-08-24 14:37:48 +0800

    Add interface for ISyncable and base class item

---
+++ Interface/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Interface
+{
+    public class Item
+    {
+        protected string id;
+
+        public Item()
+        {
+            id = Guid.NewGuid().ToString();
+        }
+    }
+}
