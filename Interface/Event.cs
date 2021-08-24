8ac50110005f0e3274e094d80c4ac9e2eb0efae6
Xinyi <xinzwerkz@gmail.com>
2021-08-24 15:52:58 +0800

    Add Event class and complete implementation

---
+++ Interface/Event.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Interface
+{
+    public class Event : Item, ISyncable
+    {
+        private string title;
+        private DateTime date;
+
+        public Event(string title, DateTime date) : base()
+        {
+            this.title = title;
+            this.date = date;
+        }
+
+        public string GetSyncString()
+        {
+            return $"EV*{id}*{title}*{date.ToString()}";
+        }
+    }
+}
