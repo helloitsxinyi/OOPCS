7be89d2d4dcc7888913ca496ad5258cf34f448bb
Xinyi <xinzwerkz@gmail.com>
2021-08-24 11:07:46 +0800

    Add AbstractClass project

---
+++ AbstractClass/Rectangle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace AbstractClass
+{
+    public class Rectangle : Shape
+    {
+        private double width, height;
+                
+        public Rectangle(string color, double width, double height): base(color)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public override double Area()
+        {
+            return width * height;
+        }
+
+
+    }
+}
---
+++ AbstractClass/Shape.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace AbstractClass
+{
+    public abstract class Shape
+    {
+        private string color;
+
+        public Shape(string color)
+        {
+            this.color = color;
+        }
+
+        // providing a "core" function for shape
+        public string GetColor()
+        {
+            return color;
+        }
+
+        // leave calculation of area to child classes
+        public abstract double Area();
+    }
+}
