7bcd38c274570989321ff6219d3fcc1a0ef8613d
Xinyi <xinzwerkz@gmail.com>
2021-08-24 15:53:15 +0800

    Add test case for main

--- Interface/Program.cs
+++ Interface/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Interface
 {
@@ -6,7 +7,22 @@ namespace Interface
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<ISyncable> syncables = new List<ISyncable>();
+
+            // simulate items fetched from user's calendar.
+            syncables.Add(new Email("Hi", "jane@gmail.com", "Free for lunch?"));
+            syncables.Add(new Contact("Mary", "90111111"));
+            syncables.Add(new Contact("Harry", "90111112"));
+            syncables.Add(new Event("Lunch with Jane", new DateTime(2021, 9, 15, 19, 0, 0)));
+
+            String serializedSync = "";
+            foreach (ISyncable item in syncables)
+            {
+                serializedSync += item.GetSyncString();
+            }
+
+            // send serialized string to server
+            Console.WriteLine(serializedSync);          
         }
     }
 }
