049618c2113eae0c08d4f90e1975d26e76d1a548
Xinyi <xinzwerkz@gmail.com>
2021-08-24 11:19:16 +0800

    Add test cases

--- AbstractClass/Program.cs
+++ AbstractClass/Program.cs
@@ -6,7 +6,18 @@ namespace AbstractClass
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int width = 5;
+            int height = 10;
+            int radius = 2;
+
+            Shape rectangle = new Rectangle("blue", width, height);
+            Console.WriteLine("Area of Rectangle: " + rectangle.Area());
+
+            Shape triangle = new Triangle("green", width, height);
+            Console.WriteLine("Area of Triangle: " + triangle.Area());
+
+            Shape circle = new Circle("yellow", radius);
+            Console.WriteLine("Area of Circle: " + $"{circle.Area():#.##}");
         }
     }
 }
