2ff15d5f63c164e27eb9b10ee5cf7084ee617fe6
Xinyi <xinzwerkz@gmail.com>
2021-08-26 12:12:50 +0800

    Add TestPropogation project

---
+++ TestPropogation/TestPropogation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestPropogation
+{
+    class TestPropogation
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
