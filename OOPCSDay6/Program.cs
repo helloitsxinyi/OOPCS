e9c4eac6432ad963d1289e4bc9e6745d4e63b95a
Xinyi <xinzwerkz@gmail.com>
2021-08-26 10:11:33 +0800

    Add OOPCSDay6 package

---
+++ OOPCSDay6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OOPCSDay6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter a divisor");
+                string input = Console.ReadLine();
+                int divisor = int.Parse(input);
+                int quotient = 10 / divisor;
+                Console.WriteLine(quotient);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }            
+        }
+    }
+}
